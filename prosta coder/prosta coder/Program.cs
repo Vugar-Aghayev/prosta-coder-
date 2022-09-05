de9635450f5b4695667265ce88690c1dc0ad28f7
Vugar Agayev <agayev.vugar2@gmail.com>
2022-09-05 17:29:05 +0400

    initial commit

---
+++ prosta coder/prosta coder/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace prosta_coder
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+
+            string name = string.Empty;
+
+            string namesasfn = "nfsaklgnask;gnb";
+        }
+    }
+}
